ff9406bac8261e3db4b385dac451f36d43fe1a3c
Oscar Francisco Meléndez Henríquez <oscarhenriquez706@gmail.com>
2024-08-13 19:59:35 -0600

    Creacion de index.php con estructura html

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>OSCAR FRANCISCO MELÉNDEZ HENRÍQUEZ</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="js/jquery-3.7.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+
+    </head>
+
+    <body>
+        
+    </body>
+</html>
